47ffa721a31c4babe4a679e2d345baed08deb4c9
Aleyna Polat <aleynapolat7424@gmail.com>
2021-12-07 20:37:59 +0300

    Initial Commit
    Basit Bir Client Server  Uygulaması

---
+++ Client/Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            socket1.Connect(IPAddress.Parse("127.0.0.1"), 600);
+            byte[] buffer = Encoding.ASCII.GetBytes(Console.ReadLine());
+            socket1.Send(buffer);
+            socket1.Close();
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                Socket socket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                socket1.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 600));
+                socket1.Listen(9);
+                Socket client = socket1.Accept();
+                NetworkStream ns = new NetworkStream(client);
+                StreamReader sr = new StreamReader(ns);
+                Console.WriteLine(sr.ReadToEnd());
+               //sr.Close;
+               //ns.Close;
+                socket1.Shutdown(SocketShutdown.Receive);
+                client.Shutdown(SocketShutdown.Receive);
+            }
+            catch (SocketException exc)
+            {
+                Console.WriteLine("error");
+
+            }
+            Console.Read();
+        }
+    }
+}
